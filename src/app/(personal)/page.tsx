4ef1f3eb6d2dabf00bd970011d79c2c60918c3c0
fajarutamaa <fajardwiutomo75@gmail.com>
2024-11-23 15:32:10 +0700

    fix(personal): updates homepage content and fix width

--- src/app/(personal)/page.tsx
+++ src/app/(personal)/page.tsx
@@ -3,20 +3,20 @@ export const dynamic = "force-static";
 
 export default function Home() {
   return (
-    <main className="container max-w[680px] leading-relaxed antialiased pb-20">
+    <main className="container max-w-[680px] leading-relaxed antialiased pb-20">
       <Link href="/" className="font-medium">
         Fajar Dwi Utomo
       </Link>
       <p className="opacity-70 font-medium">Junior Software Engineer 👋</p>
       <p className="mt-6 text-foreground/70">
-        I'am a Software engineer passionate about building things people love.
-        Currently learning and working on{" "}
+        I'am a junior software engineer passionate about building things people
+        love. Currently learning and working on{" "}
         <Link
           href="https://gps.id"
           className="hover:text-[#1da1f2]"
           target="_blank"
         >
-          GPS.id
+          GPS.id.
         </Link>
       </p>
     </main>
